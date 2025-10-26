ccfcf8ce77d91dc7c9eeb9683bc9f9cc7d0594bf
mariam <mariamemampp00@gmail.com>
2025-10-26 17:07:32 +0300

    handle permission resource

---
+++ app/Filament/Resources/Permissions/Schemas/PermissionForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\Permissions\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class PermissionForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                 TextInput::make('name')
+                ->minLength(2)
+                ->maxLength(255)
+                ->required()
+                ->unique(),
+            ]);
+    }
+}
